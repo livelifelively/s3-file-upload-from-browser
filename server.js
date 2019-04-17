3b70a9b676b4de9cb0201d0dedc1786c9ff6d6e9
ashish rana <ashish@msg.ai>
2019-04-17 06:54:41 +0530

    generate presigned putObject url

--- server.js
+++ server.js
@@ -1,9 +1,53 @@
 const Koa = require('koa');
+const Router = require('koa-router');
 const app = new Koa();
+var router = new Router();
+const aws = require('aws-sdk')
+const config = require('./config')
+const axios = require('axios');
+const uuid = require('uuid')
+
+console.log(config)
+
+aws.config = new aws.Config({
+  accessKeyId: config.ACCESS_KEY_ID,
+  secretAccessKey: config.SECRET_ACCESS_KEY
+})
+
+const client = new aws.S3()
  
-// response
-app.use(ctx => {
-  ctx.body = 'Hello Koa';
-});
+router.get('/', (ctx, next) => {
+  const data = {
+    contentType: 'image/png'
+  }
+
+  let params = {
+    // ContentType: fileExtension(data.contentType),
+    Expires: 60 * 60,
+    // ACL: 'bucket-owner-full-control',
+    Bucket: config.BUCKET_NAME,
+    Key: randomName() + '.' + fileExtension(data.contentType)
+  }
+  console.log(params)
+  const url = client.getSignedUrl('putObject', params)
+  console.log(url)
+  ctx.body = 'Hello Koa Get';
+})
+
+router.post('/', (ctx, next) => {
+  ctx.body = 'Hello Koa Post'
+})
+
+app
+  .use(router.routes())
+  .use(router.allowedMethods());
  
 app.listen(3000)
+
+function randomName () {
+  return uuid.v4()
+}
+
+function fileExtension (contentType) {
+  return contentType.replace('image/', '')
+}
